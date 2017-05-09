8f1e6b01cc522d9f30de32eb020340de49c11d59
Stas_Berko <stas.berko@yandex.ru>
2017-05-10 00:09:28 +0300

    WorkVersion (v0.02) NewGroup

--- src.py
+++ src.py
@@ -4,7 +4,6 @@ import json
 import requests
 import os
 import re
-import time
 from lxml import html
 from login import *
 
@@ -26,14 +25,17 @@ def get_GoogleInfo(as_q="Python", num = "3"):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
     dat = dict(hl="ru", as_qdr="all", as_occt="any", safe="images", as_q=as_q, num=str(num))
     res = requests.get(url, params=dat, headers=headers)
-    tsiu = []
-    tm_dct = {}
+    t_dct={}
     tree = html.fromstring(res.content.decode())
-    tm_dct['url'] = tree.xpath('//h3[@class = "r"]/a/@href')
-    tm_dct['title'] = tree.xpath('//h3[@class = "r"]/a/text()')
-    tm_dct['short_info'] = tree.xpath('//span[@class = "st"]/text()')
-    tsiu.append(tm_dct.copy())
-    return tsiu
+    for i in range(int(num)):
+        try:
+            t_dct[i] = \
+                (tree.xpath('//h3[@class = "r"]/a/text()')[i],
+                 tree.xpath('//span[@class = "st"]/text()')[i],
+                 tree.xpath('//h3[@class = "r"]/a/@href')[i])
+        except IndexError:
+            pass
+    return t_dct
 
 
 def get_GooglePicture(as_q="Python"):
@@ -55,41 +57,56 @@ def send_photo(photo_name, user_id=my_id):
     for i, img in enumerate(photo_name):
         upl = requests.get(img)
         new_name = str(i)+os.path.splitext(img)[1]
-        new_name = re.findall(r"([\d]?\.(jpeg|png|jpg|gif))", new_name)[0][0]###################################################################
-        with open(new_name, "wb") as fl:
-            fl.write(upl.content)
         try:
-            upld = upload.photo_messages(new_name)[0]
-        except vk_api.exceptions.ApiError:
+            res = re.search(r"[\d]*[.](jpeg|png|jpg|gif)", new_name)###################################################################
+            new_name = res.group(0)
+        except AttributeError:
             pass
         else:
-            os.remove(new_name)
-            photo_mas.append("photo{}_{}".format(upld['owner_id'], upld['id']))
-    vk.method('messages.send', {'user_id': user_id, 'attachment': ",".join(photo_mas)})
+            with open(new_name, "wb") as fl:
+                fl.write(upl.content)
+            try:
+                upld = upload.photo_messages(new_name)[0]
+            except vk_api.exceptions.ApiError:
+                pass
+            else:
+                os.remove(new_name)
+                photo_mas.append("photo{}_{}".format(upld['owner_id'], upld['id']))
+    if photo_mas:
+        vk.method('messages.send', {'user_id': user_id, 'attachment': ",".join(photo_mas)})
+    else:
+        write_msg(user_id, "Не вдалося знайти жодного зображення")
 
 
 def run():
-    values = {'out': 0, 'count': 1, 'time_offset': 30}
+    values = {'out': 0, 'count': 10, 'time_offset': 30}
     while True:
         response = vk.method('messages.get', values)
         if response['items']:
             values['last_message_id'] = response['items'][0]['id']
-            print(response['items'])
-        for item in response['items']:
-            info = get_GoogleInfo(item["body"])[0]
-            img = get_GooglePicture(item["body"])
-            for i in range(len(info)-1):
-                write_msg(item['user_id'], "*{} / {} / {}".format(info['title'][i], info['short_info'][i], info['url'][i]))
-            send_photo(img, item['user_id'])
-        time.sleep(0.5)
+            print(response['items'][0])
+            for item in response['items']:
+                if not is_user_memb(item["user_id"]):
+                    write_msg(item["user_id"],
+                    "Привіт, бот доступний тільки для підписників, спочатку підпишись https://vk.com/ukcht а потім напиши знову)) 😃 Команда UkrChat 🖐🏻")
+                else:
+                    info = get_GoogleInfo(item["body"])
+                    img = get_GooglePicture(item["body"])
+                    if info:
+                        for i in info:
+                            write_msg(item['user_id'], "* {} / {} / {}".format(info[i][0], info[i][1], info[i][2],))
+                    else:
+                        write_msg(item['user_id'], "Не вдалося знайти інформацію")
+                    if img:
+                        send_photo(img, item['user_id'])
+                    else:
+                        write_msg(item['user_id'], "Не вдалося знайти жодного зображення")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     vk = vk_api.VkApi(token=token)
     vk.auth()
     upload = vk_api.VkUpload(vk)
     run()
-
-
 
 
 
